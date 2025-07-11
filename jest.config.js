c15f7ecf9db8749de614d244fde7749c2e6b618c
alfiemohamad <142969609+alfiemohamad@users.noreply.github.com>
2025-07-11 21:46:16 +0700

    uploud to codecove

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'html'],
+  coverageReporters: ['lcov', 'text'],
+  testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
+  roots: ['<rootDir>/src'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
